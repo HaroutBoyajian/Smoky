1cac74e4222c9e829dfb79b1e7e44e20f25d7fe8
Harout Boyajian <harouth94@gmail.com>
2024-05-22 14:29:14 +0400

    Created Card script and added to card prefabs

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable] 
+public class Card : MonoBehaviour
+{
+    public Sprite cardBackImage;
+    public Sprite cardFrontImage;
+    public GameObject cardBackText;
+    public GameObject cardFrontText;
+}
